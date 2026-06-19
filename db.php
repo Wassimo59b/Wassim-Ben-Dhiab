06b71d776c0d0c3b2ae04fb79d84e807168ebeea
Wassimo59b <135971076+Wassimo59b@users.noreply.github.com>
2026-06-19 17:33:23 +0100

    db.php

--- db.php
+++ db.php
@@ -1,13 +1,15 @@
 <?php
-$host = getenv("railway");
-$user = getenv("root");
-$password = getenv("DNrSuFsqEryhWioWJADhOazXYSTbbSvv");
-$database = getenv("railway");
-$port = getenv("3306");
+
+$host = getenv("MYSQLHOST");
+$user = getenv("MYSQLUSER");
+$password = getenv("MYSQLPASSWORD");
+$database = getenv("MYSQLDATABASE");
+$port = getenv("MYSQLPORT");
 
 $conn = new mysqli($host, $user, $password, $database, $port);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
 ?>
